67a46574b0487585d311f743a911f3f07c308344
gab <gabriel.pontarolo045@gmail.com>
2023-02-15 20:27:12 -0300

    print head para a alocacao consecutiva

--- src/c_imp/alocacao_consecutiva/main.c
+++ src/c_imp/alocacao_consecutiva/main.c
@@ -2,11 +2,29 @@
 #include "alocador.h"
 #include <string.h>
 
+void printHeap(void)
+{
+    unsigned char *aux_ptr = initial_top;
+    long long counter = 0;
+
+    while (aux_ptr < (unsigned char *)current_top)
+    {
+        if ((counter % 8) == 0)
+        {
+            printf("\n%p : ", aux_ptr);
+        }
+        printf("0x%02x ", *(aux_ptr));
+        aux_ptr++;
+        counter++;
+    }
+    printf("\n");
+}
+
 // algoritmo da secao 6.2
 int main(int argc, char **argv)
 {
     void *a;
-       
+
     printf("\n"); // coloca o buffer do printf antes do alocador
     iniciaAlocador();
 
@@ -18,6 +36,7 @@ int main(int argc, char **argv)
         printf("%p %s \n", a, (char *)a);
         liberaMem(a);
     }
+    printHeap();
 
     finalizaAlocador();
 
--- src/c_imp/best_fit/alocador.h
+++ src/c_imp/best_fit/alocador.h
@@ -8,6 +8,5 @@ void iniciaAlocador(void);
 void finalizaAlocador(void);
 void *alocaMem(long long num_bytes);
 int liberaMem(void *bloco);
-void printHeap(void);
 
 #endif
