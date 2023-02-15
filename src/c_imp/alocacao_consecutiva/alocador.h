90f3fe9cc3d158ec14f22bddb6fe9fb10d2dc3fd
Gabp0 <gabriel.pontarolo045@gmail.com>
2023-02-15 14:21:33 -0300

    alocacao consecutiva

---
+++ src/c_imp/alocacao_consecutiva/alocador.h
@@ -0,0 +1,13 @@
+#ifndef __ALOCADOR__
+#define __ALOCADOR__
+
+extern void *initial_top;
+extern void *current_top;
+
+void iniciaAlocador(void);
+void finalizaAlocador(void);
+void *alocaMem(long long num_bytes);
+int liberaMem(void *bloco);
+void printHeap(void);
+
+#endif
---
+++ src/c_imp/alocacao_consecutiva/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "alocador.h"
+#include <string.h>
+
+// algoritmo da secao 6.2
+int main(int argc, char **argv)
+{
+    void *a;
+       
+    printf("\0"); // coloca o buffer do printf antes do alocador
+    iniciaAlocador();
+
+    int i;
+    for (i = 0; i < 100; i++)
+    {
+        a = alocaMem(100);
+        strcpy(a, " TESTE ");
+        printf("%p %s \n", a, (char *)a);
+        liberaMem(a);
+    }
+
+    finalizaAlocador();
+
+    return (0);
+}
