665397affce72750dc51e951d0b120db104d2000
Casadororo <51424037+Casadororo@users.noreply.github.com>
2023-02-14 21:34:56 -0300

    Revert "garb"
    This reverts commit 3c7df57319a2ffe340632c272f8b6371ee62890f.

--- src/alocador.c
+++ src/alocador.c
@@ -1,22 +1,25 @@
 #include "alocador.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 void *topo_inicial;
 
 void iniciaAlocador(void)
 // salva o topo inicial da heap
 {
-    topo_inicial = 0;
+    topo_inicial = sbrk(0);
 }
 
 void finalizaAlocador(void)
 // restaura o topo da heap inicial
 {
-    topo_inicial = 0;
+    brk(topo_inicial);
 }
 
 void *firstFit(long long int num_bytes)
 {
-    void *topo_atual = 0;
+    void *topo_atual = sbrk(0);
     char *aux_ptr = topo_inicial;
 
     // procura por um bloco vazio com um tamanho >= a num_bytes
@@ -32,7 +35,47 @@ void *firstFit(long long int num_bytes)
     // se nao achou, aloca um novo
     if (aux_ptr == topo_atual)
     {
-        void *topo_atual = 0;;
+        sbrk(num_bytes + 5);
+    }
+    // divide o bloco, caso tenha mais bytes
+    else if (*((int *)(aux_ptr + 1)) > num_bytes)
+    {
+        unsigned int size = *(int *)(aux_ptr + 1) - num_bytes - 5;
+        *((int *)(aux_ptr + num_bytes + 5)) = 0;
+        *((int *)(aux_ptr + num_bytes + 6)) = size;
+    }
+
+    *aux_ptr = 1; // bloco ocupado
+    aux_ptr++;
+    *((int *)aux_ptr) = num_bytes; // tamanho do bloco
+    aux_ptr += 4;                  // inicio dos conteudo
+
+    return aux_ptr;
+}
+
+void *bestFit(int num_bytes)
+{
+    void *topo_atual = sbrk(0);
+    char *aux_ptr = topo_inicial;
+
+    // procura pelo menor bloco vazio com um tamanho >= a num_bytes
+    void *best_ptr = aux_ptr;
+    int best_size = num_bytes + 1;
+    while (aux_ptr < (char *)topo_atual)
+    {
+        int current_block_size = *((int *)(aux_ptr + 1));
+        if ((*aux_ptr == 0) && (current_block_size >= num_bytes) && (current_block_size < best_size))
+        {
+            best_ptr = aux_ptr;
+        }
+        aux_ptr += current_block_size + 5;
+    }
+    aux_ptr = best_ptr;
+
+    // se nao achou, aloca um novo
+    if (aux_ptr == topo_atual)
+    {
+        sbrk(num_bytes + 5);
     }
     // divide o bloco, caso tenha mais bytes
     else if (*((int *)(aux_ptr + 1)) > num_bytes)
